d81703e1c67833a9090f45a38830636b5aa472a4
Sameera Wijesekara <sameerawijesekara2001@gmail.com>
2025-02-18 11:04:56 +0530

    component change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar() {
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
-            <Link to="/favorites" className="nav-link">Favorites</Link>
+            <Link to="/favorites" className="nav-link">My Favorites</Link>
         </div>
     </nav>
 }
